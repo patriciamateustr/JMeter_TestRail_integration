c4574ad45fc54ea0981bd4113ea76f6f9722d628
Patrícia Duarte Mateus <patricia.mateus@testrail.com>
2024-12-30 16:01:51 +0000

    first commit code sample

---
+++ jmeter_TR_integration/jmeter_trcli.sh
@@ -0,0 +1,27 @@
+clear
+bold=$(tput bold)
+TRCLI_CONFIG_PATH="C:\Users\patri\Documents\GitRepo\Performance Samples\jmeter_TR_integration\trcli-config.yml"
+JMETER_RESULTS="C:\Program Files\apache-jmeter-5.6.3\bin\samples\sample_results.xml"
+
+echo "($bold)-------------------------------------"
+echo "($bold)--- JMeter TestRail Sample Project --"
+echo "($bold)-------------------------------------"
+
+echo "($bold)... verify if TRCLI is installed:"
+echo
+
+trcli
+echo
+
+echo "($bold)... execute JMeter Load Test:"
+echo
+./jmeter.bat --nongui --testfile "jmeter_TR_integration\jmeter_perf_test.jmx"
+echo
+
+
+echo "($bold)... executing TestRail TRCLI to upload results in a new Test Run:"
+echo
+trcli --yes --config "$TRCLI_CONFIG_PATH" parse_junit --file "$JMETER_RESULTS" --title "JMeter Demo"
+echo
+
+echo "($bold)... execution completed. New Test Run created."
